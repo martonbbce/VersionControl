b67f96ce8e5105251823c6f05aa856b17c13ed43
Márton Bozai <marton.bozai@stud.uni-corvinus.hu>
2022-11-12 00:15:22 +0100

    commit new

---
+++ UserMaintenanceFour/Entities2/UserSecondTry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace UserMaintenanceFour.Entities
+{
+	public class UserSecondTry
+	{
+		public Guid ID { get; set; } = Guid.NewGuid();
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string FullName
+		{
+			get
+			{
+				return string.Format("{0} {1}", LastName, FirstName);
+			}
+		}
+		public UserSecondTry()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
