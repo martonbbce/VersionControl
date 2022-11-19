9c2d1f9f58414a4430b384dedc99a0373812d2aa
martonbbce <marton.bozai@stud.uni-corvinus.hu>
2022-11-19 17:59:46 +0100

    7. rész

--- RealEstateProject/RealEstateProject/Form1.cs
+++ RealEstateProject/RealEstateProject/Form1.cs
@@ -24,6 +24,7 @@ namespace RealEstateProject
             InitializeComponent();
             LoadData();
             CreateExcel();
+            //MessageBox.Show(GetCell(2, 2), "2, 2 eredménye");
         }
 
         void LoadData()
@@ -38,7 +39,7 @@ namespace RealEstateProject
                 xlApp = new Excel.Application();
                 xlWB = xlApp.Workbooks.Add(Missing.Value);
                 xlSheet = xlWB.ActiveSheet;
-                // CreateTable();
+                CreateTable();
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
             }
@@ -52,6 +53,86 @@ namespace RealEstateProject
                 xlApp = null;
             }
             
+        }
+
+        void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+            }
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int index = 0;
+            foreach (Flat f in Flats)
+            {
+                values[index, 0] = f.Code;
+                values[index, 1] = f.Vendor;
+                values[index, 2] = f.Side;
+                values[index, 3] = f.District;
+                values[index, 4] = f.Elevator;
+                values[index, 5] = f.NumberOfRooms;
+                values[index, 6] = f.FloorArea;
+                values[index, 7] = f.Price;
+                values[index, 8] = "";
+                index++;
+            }
+
+            xlSheet.get_Range(
+                GetCell(2, 1), 
+                GetCell(1+values.GetLength(0),values.GetLength(1))
+                ).Value2=values;
+            string[] kepletesoszlop = new string[Flats.Count];
+            index = 0;
+            string arcella;
+            string teruletcella;
+            foreach (string item in kepletesoszlop)
+            {
+                arcella = GetCell(index + 1, 8);
+                teruletcella = GetCell(index + 1, 7);
+                kepletesoszlop[index] = "=1000000*"+arcella+"/" + teruletcella;
+                
+                //kepletesoszlop[index] = String.Format("={1}/{2}", GetCell(index+1, 8), GetCell(index+1,7));
+                Console.WriteLine(kepletesoszlop[index]);
+                index++;
+            }
+            //while (index < kepletesoszlop.Length)
+            //{
+            //    kepletesoszlop[index] = String.Format("={1}/{2}", GetCell(index + 1, 8), GetCell(index + 1, 7));
+            //    Console.WriteLine(kepletesoszlop[index]);
+            //    index++;
+            //}
+            xlSheet.get_Range(GetCell(2,9),GetCell(kepletesoszlop.Length+1,9)).Value = kepletesoszlop;
+
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+            return ExcelCoordinate;
         }
     }
 }
