a37c84409841d082896848b2127f24bb4a99de48
martonbbce <marton.bozai@stud.uni-corvinus.hu>
2022-11-26 10:07:39 +0100

    week05_05_mukodik

--- WebServiceGyak05/WebServiceGyak05/Form1.cs
+++ WebServiceGyak05/WebServiceGyak05/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using WebServiceGyak05.Entities;
 
 
@@ -18,10 +19,14 @@ namespace WebServiceGyak05
         BindingList<RateData> Rates;
         public Form1()
         {
-            LoadMnbServices();
             InitializeComponent();
+            Rates = new BindingList<RateData>();
+            dataGridView1.DataSource = Rates;
+            
+            DoXml();
+            
         }
-        void LoadMnbServices()
+        private static string LoadMnbServices()
         {
             var mnbService = new MnbServiceReference.MNBArfolyamServiceSoapClient();
             var request = new MnbServiceReference.GetExchangeRatesRequestBody()
@@ -32,9 +37,26 @@ namespace WebServiceGyak05
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
-            File.WriteAllText("kimenet.txt", result);
-            Rates = new BindingList<RateData>();
-            dataGridView1.DataSource = Rates;
+            File.WriteAllText("kimenet"+DateTime.Now.ToString("yyMMddHHmm"), result);
+            //Rates = new BindingList<RateData>();
+            return result;
+        }
+        void DoXml()
+        {
+            XmlDocument xmldocc = new XmlDocument();
+            xmldocc.LoadXml(LoadMnbServices());
+            foreach (XmlElement el in xmldocc.DocumentElement)
+            {
+                RateData rd = new RateData();
+                Rates.Add(rd);
+                rd.Date = DateTime.Parse(el.GetAttribute("date"));
+                var childElement = (XmlElement)el.ChildNodes[0];
+                rd.Currency = childElement.GetAttribute("curr");
+                var unitt = decimal.Parse(childElement.GetAttribute("unit"));
+                var valuee = decimal.Parse(childElement.InnerText);
+                if (unitt != 0)
+                    rd.Value = valuee / unitt;
+            }
         }
     }
 }
