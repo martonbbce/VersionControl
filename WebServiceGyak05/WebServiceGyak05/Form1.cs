3bd7390bd42ec4bce4f76aaed0d2dc16181eba33
martonbbce <marton.bozai@stud.uni-corvinus.hu>
2022-11-26 09:09:19 +0100

    week05-03

--- WebServiceGyak05/WebServiceGyak05/Form1.cs
+++ WebServiceGyak05/WebServiceGyak05/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,21 @@ namespace WebServiceGyak05
         public Form1()
         {
             InitializeComponent();
+            LoadMnbServices();
         }
         void LoadMnbServices()
         {
             var mnbService = new MnbServiceReference.MNBArfolyamServiceSoapClient();
-
+            var request = new MnbServiceReference.GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate="2022-01-01",
+                endDate="2022-10-31"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            File.WriteAllText("kimenet.txt", result);
+            
         }
     }
 }
